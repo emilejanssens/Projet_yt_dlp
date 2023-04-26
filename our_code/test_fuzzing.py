e861469a4b155241153b96595a9615ef4034a0ae
ton-s <sohyantonin@gmail.com>
2023-04-26 18:14:50 +0200

    new fuzzing

--- our_code/test_fuzzing.py
+++ our_code/test_fuzzing.py
@@ -1,106 +1,83 @@
 import subprocess
+import random
+import string
 
-# Valid inputs
-inputs = [
-    "https://www.youtube.com/watch?v=pRpeEdMmmQ0",
-    "https://www.youtube.com/watch?v=F_mqShCzlAY",
-    "https://www.youtube.com/watchv=F_mqShCzlAY",
-    'https://www.youtube.com/watch?v=F_mqShCzlAY"&"list=PLhQjrBD2T382hjVvKtTiBQYvDxGDQyU4p',
-    "https://player.vimeo.com/video/54469442",
-    "https://player.vimeo.com/video/54469442/",
+# Nombre d'itérations pour tester le programme
+num_iter = 10
+
+# Fonctions pour générer une URL aléatoire
+
+def generate_url(id_size, str_url):
+    # Générer un identifiant vidéo YouTube aléatoire
+    video_id = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(id_size))
     
-    "https://watch.pokemon.com/fr-fr/#/player?id=c0d71edb6ec243ae94718327637ac46c/",
-    "https://watch.pokemon.com/fr-fr/#/player?id=c0d71edb6ec243ae94718327637ac46c/test",
-    "https://watch.pokemon.com/fr-fr/#/player?id=c0d71edb6ec243ae94718327637ac46c",
+    return str_url + video_id
+    
+    
+# Fonction pour muter une URL
+def mutate_url(url):
+    # Convertir l'URL en une liste de caractères pour pouvoir la modifier
+    url_list = list(url)
+    
+    # Choisir aléatoirement le type de mutation à effectuer
+    type_mutation = random.choice(["change", "delete", "add"])
+    
+    if type_mutation == "change":
+        # Choisir un caractère aléatoire à remplacer
+        index = random.randint(0, len(url_list) - 1)
+        
+        # Remplacer le caractère par un autre caractère aléatoire
+        url_list[index] = random.choice(string.ascii_lowercase + string.digits)
+        
+    elif type_mutation == "delete":
+        # Choisir un caractère aléatoire à supprimer
+        index = random.randint(0, len(url_list) - 1)
+        
+        # Supprimer le caractère de la liste
+        url_list.pop(index)
+        
+    else :
+        # Choisir un caractère aléatoire à ajouter
+        char = random.choice(string.ascii_lowercase + string.digits)
+        
+        # Choisir un index aléatoire pour ajouter le caractère
+        index = random.randint(0, len(url_list))
+        
+        # Ajouter le caractère à la liste
+        url_list.insert(index, char)
+    
+    # Rejoindre les caractères pour former une nouvelle URL mutée
+    mutated_url = ''.join(url_list)
+    
+    return type_mutation, mutated_url
 
-    "https://www.instagram.com/stories/hugodecrypte/3080791283912358444/",
-    "https://www.instagram.com/stories/hugodecrypte/3080791283912358444",
-    "https://www.instagram.com/stories/hugodecrypte/3080791283912358444/test",
-    "https://www.instagram.com/p/Cpk_IWlDt21/",
-    "https://www.instagram.com/p/Cpk_IWlDt21",
-    "https://www.instagram.com/p/Cpk_IWlDt21/test",
-
-    "https://www.facebook.com/fcbarcelona/videos/182599291408939",
-    "https://www.facebook.com/fcbarcelona/videos/182599291408939/",
-    "https://www.facebook.com/fcbarcelona/videos/182599291408939/test",
-    "https://www.facebook.com/watch/?v=1363029404119602",
-    "https://www.facebook.com/watch/?v=1363029404119602/",
-    "https://www.facebook.com/reel/1195014674539147/?s=single_unit",
-    "https://www.facebook.com/reel/1195014674539147/?s=single_unit/",
-
-    "https://www.dailymotion.com/video/x8k2sh7?playlist=x6bgai",
-    "https://www.dailymotion.com/video/x8k2sh7?playlist=x6bgai/",
-    "https://www.dailymotion.com/video/x8k1mpj",
-    "https://www.dailymotion.com/video/x8k1mpj/",
-
-    "https://www.formula1.com/en/latest/video.in-depth-sergio-perez-reflects-on-his-time-with-racing-point.1687525282173377683.html",
-    "https://www.formula1.com/en/latest/video.in-depth-sergio-perez-reflects-on-his-time-with-racing-point.1687525282173377683.html/",
-
-    "https://www.nintendo.com/nintendo-direct/03-09-2023/",
-    "https://www.nintendo.com/nintendo-direct/03-09-2023",
-
-    # erreur susceptible de Facebook
-    "https://m.facebook.com/stories/view_tray_pagination/1694351550654428/?tray_session_id=f3f6199a-d2d4-4d88-9906-1a47834e22f1&thread_id=782399996346255&end_cursor=MTAwMDA5NzcwNjQ5NTQyOjE6MTY4MTU1NTM5MDoxNjg3MTY5Njc5NTQzODE6LTE6Y2xuOjI5OTc0MjQwOTA5NjA0MTc4NTM6MDoxNjgxNTU1MzkwOg%3D%3D&has_next_page=false",
-
-    "https://drive.google.com/file/d/1BiLl5grkxDNXVUvkBJanA-0d_Q0sfUBT/view/",
-    "https://drive.google.com/file/d/1BiLl5grkxDNXVUvkBJanA-0d_Q0sfUBT/view",
-    "https://drive.google.com/file/d/1BiLl5grkxDNXVUvkBJanA-0d_Q0sfUBT/youyou",
-    "https://drive.google.com/file/d/1BiLl5grkxDNXVUvkBJanA-0d_Q0sfUBT",
-
-    "https://www.tiktok.com/@reussironly/video/7221968120259366150?is_from_webapp=1&sender_device=pc",
-    "https://www.tiktok.com/@reussironly/video/7221968120259366150a",
-    "https://www.tiktok.com/@reussironly/video/7221968120259366150/",
-
-    "https://www.deezer.com/fr/album/350891827",
-    "https://www.deezer.com/fr/album/350891827/",
-    "https://www.deezer.com/XX/album/350891827",
-    "https://www.deezer.com////album/350891827",
-    "https://www.deezer.com/fr/playlist/10783752422",
-    "https://www.deezer.com/&#/playlist/10783752422",
-    "https://www.deezer.com/fr/playlist/10783752422/",
-
-    "https://www.amazon.aaa.bb/dp/B098XNCHLD/?th=1",
-    "https://www.amazon.aaa.bb/dp/B098XNCHLD###&&&",
-    "https://www.amazon.com/gp/customer-reviews/R10VE9VUSY19L3/ref=cm_cr_arp_d_rvw_ttl",
-    "https://www.amazon.com/gp/customer-reviews/R10VE9VUSY19L3##&&",
-
-    "https://www.reddit.com/r/interestingasfuck/comments/12mjhdz/worst_pain_known_to_man/",
-    "https://www.reddit.com/r/interestingasfuck/comments/12mjhdz#",
-
-    # erreur eurosport
-    "https://www.eurosport.com/football/champions-league/2022-2023/pep-guardiola-emotionally-destroyed-after-manchester-city-win-over-bayern-munich-in-champions-league_vid1896254/video.shtml",
-    "https://www.eurosport.com/football/champions-league/20222023/pep-guardiola-emotionally-destroyed-after-manchester-city-win-over-bayern-munich-in-champions-league_vid1896254",
-
-    "https://youtu.be/dQw4w9WgXcQ",
-    "https://www.youtube.com/live/dQw4w9WgXcQ",
-    "https://www.youtube-nocookie.com/watch?v=dQw4w9WgXcQ",
-    "https://www.deturl.com/www.youtube.com/watch?v=dQw4w9WgXcQ",
-    "https://www.deturl.com/www.youtube.com/watch?v=dQw4w9WgXcQ/",
-    "https://www.pwnyoutube.com/watch?v=dQw4w9WgXcQ",
-
-    "https://www.invidio.us/playlist?list=PLVhZRy5c_nYhLrjji-jYZyKrNmOy8QvD-",
-    "https://mychannel.youtube.com/playlist?list=PLvahj0XJXycFCQ-27Y6UYYv6JdK0hNZrB/",
-    "https://www.youtube.com/playlist?list=PLvahj0XJXycFCQ-27Y6UYYv6JdK0hNZrB",
-
-    "https://www.youtube.com/results?search_query=example",
-    "https://youtube.com/search?search_query=example&lang=en",
-    "https://www.youtube.com/results?search_query=example&lang=en/s",
-
-    "https://music.youtube.com/search?search_query=example&ytsession=bVn1nETu68jVrEzZcdjb7pWiBoNoGmta",
-    "https://music.youtube.com/search?search_query=example&ab_channel=exampleChannel",
-    "http://music.youtube.com/search?q=example/"
-]
 
 COMMAND = "yt-dlp {} -P ./trash"
 
-for input_str in inputs:
+for i in range(num_iter):
+    
+    # Générer une URL aléatoire
+    url = random.choice([generate_url(11, "https://www.youtube.com/watch?v="),
+                         generate_url(8, "https://player.vimeo.com/video/"),
+                         generate_url(7, "https://www.dailymotion.com/video/"),
+                         generate_url(9, "https://www.deezer.com/fr/album/"),
+                         generate_url(11, "https://youtu.be/"),
+                         generate_url(14, "https://www.amazon.com/gp/customer-reviews/"),
+                         generate_url(20, "https://www.tiktok.com/@reussironly/video/"),
+                         ])
+
+    # Muter l'URL aléatoire
+    type_mutation, mutated_url = mutate_url(url)
+    
+    print(f"No mutated url : {url} [{type_mutation}]")
+        
     try:
         output = subprocess.check_output(COMMAND.format(
-            input_str), shell=True, stderr=subprocess.STDOUT)
-        print(f"Command succeeded with input: {input_str}")
+            mutated_url), shell=True, stderr=subprocess.STDOUT)
+        print(f"Command succeeded with input: {mutated_url}\n")
         # print(output)
 
     except subprocess.CalledProcessError as e:
 
-        print(f"Command failed with input: {input_str}")
-        print(e.output)
+        print(f"Command failed with input: {mutated_url}")
+        print(f"{e.output}\n")
